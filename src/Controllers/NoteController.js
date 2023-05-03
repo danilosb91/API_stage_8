e4bd39071836d20d069e4fc20565291279b40e22
danilosb91 <danilosilva.barao@gmail.com>
2023-05-02 21:11:31 -0300

    Update de index controller to filter by tags or title

--- src/Controllers/NoteController.js
+++ src/Controllers/NoteController.js
@@ -37,6 +37,46 @@ class NotesController {
    return response.status(201).json();
   
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex("notes").where({id}).first();
+    const tags = await knex("tags").where({note_id: id}).orderBy("name");
+    const links = await knex("links").where({note_id: id}).orderBy("created_at");
+
+    return response.json({
+     ...note,
+      tags,
+      links}
+    );
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+     await knex("notes").where({id}).delete();
+
+  return response.json()
+  }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query;
+
+    let notes;
+
+    if(tags){
+      const filterTags = tags.split(',').map(tag => tag.trim());
+      console.log(filterTags)
+      
+      notes = await knex("tags").whereIn("name", filterTags)
+
+    }else{
+       notes = await knex("notes").where({user_id}).whereLike("title",`%${title}%`).orderBy("title");
+
+    }
+
+    return response.json(notes);
+  }
 }
 
 module.exports = NotesController;
