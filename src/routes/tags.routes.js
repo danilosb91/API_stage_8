dc698ac62daa590269af1cf5b22b89e7fc9d3973
danilosb91 <danilosilva.barao@gmail.com>
2023-05-24 20:22:10 -0300

    .

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -6,7 +6,7 @@ const tagsRoutes = Router();
 
 const tagsController = new TagsController();
 
-tagsRoutes.get("/", tagsController.index);     
+tagsRoutes.get("/:user_id", tagsController.index);     
 
 
 
