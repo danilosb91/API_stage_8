7a235ada39c7522b58d40bdd8566ca83a382a2b7
danilosb91 <danilosilva.barao@gmail.com>
2023-05-24 20:16:42 -0300

    Creating the tag controller and adding it to the routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const userRoutes = require("./users.routes");
 const notesRoutes = require("./notes.routes");
 const sessionsRoutes = require("./sessions.routes");
+const tagsRoutes = require("./tags.routes");
 
 
 
@@ -11,6 +12,7 @@ const routes = Router();
 routes.use("/sessions", sessionsRoutes);
 routes.use("/users", userRoutes);
 routes.use("/notes", notesRoutes);
+routes.use("/tags", tagsRoutes);
 
 
 
