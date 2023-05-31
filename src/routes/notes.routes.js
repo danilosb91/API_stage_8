801c0569dc7cb590ea55d6c12f0d0d7cd190bf33
danilosb91 <danilosilva.barao@gmail.com>
2023-05-31 18:04:46 -0300

    update controllers to use JWT authentication

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,17 +1,17 @@
 const { Router } = require("express");
 
 const NotesController = require("../Controllers/NoteController");
+const ensureAuthenticated = require("../middlewares/middleware");
 
 const notesRoutes = Router();
 
+notesRoutes.use(ensureAuthenticated);
+
 const notesController = new NotesController();
 
-notesRoutes.get("/", notesController.index);     
-notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.post("/", notesController.create);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
-
-
-
+notesRoutes.get("/", notesController.index);
 
 module.exports = notesRoutes;
--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 
 const TagsController = require("../Controllers/TagsController");
+const ensureAuthenticated = require("../middlewares/middleware");
+
 
 const tagsRoutes = Router();
 
 const tagsController = new TagsController();
 
-tagsRoutes.get("/:user_id", tagsController.index);     
+tagsRoutes.get("/",ensureAuthenticated, tagsController.index);     
 
 
 
