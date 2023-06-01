9dd964cbf43821cd2607bb7b0dca288a66c1ea9e
danilosb91 <danilosilva.barao@gmail.com>
2023-05-31 21:42:29 -0300

    .

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,23 +3,18 @@ const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const UserController = require("../Controllers/UserController");
+const UserAvatarController = require("../Controllers/UserAvatarController");
+
 const ensureAuthenticated = require("../middlewares/middleware");
 
 const userRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const userController = new UserController();
+const userAvatarController = new UserAvatarController();
 
 userRoutes.post("/", userController.create);
 userRoutes.put("/", ensureAuthenticated, userController.update);
-userRoutes.patch(
-  "/avatar",
-  ensureAuthenticated,
-  upload.single("avatar"),
-  (request, response) => {
-    console.log(request.file.filename);
-    response.json();
-  }
-);
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 module.exports = userRoutes;
