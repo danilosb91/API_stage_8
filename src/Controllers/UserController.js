5abdb3fb25d911bde6d642073fee009b580aef22
danilosb91 <danilosilva.barao@gmail.com>
2023-07-18 20:11:48 -0300

    Install Jest auto teste

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,15 +1,18 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
-const { json } = require("express");
+
+const  UserRepository  = require("../repositories/userRepository");
 
 class UserController {
   async create(request, response) {
     const { name, email, password } = request.body;
 
-    const database = await sqliteConnection();
+    const userRepository = new UserRepository();
 
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)",[email])
+
+    const checkUserExists = await userRepository.findByEmail(email);
+  
 
     if (checkUserExists) {
       throw new AppError("Este e-mail já está em uso.");
@@ -17,10 +20,9 @@ class UserController {
 
     const hashedPassword = await hash(password, 8);
 
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?,?,?)",
-      [name, email, hashedPassword]
-    );
+    await userRepository.create({name, email, password:hashedPassword});
+
+
     return response.status(201).json();
   }
 
@@ -28,9 +30,10 @@ class UserController {
     const { name, email, password, old_password } = request.body;
     const user_id = request.user.id;
 
-
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ]);
 
     if (!user) {
       throw new AppError("Usuário não encontrado!");
@@ -40,13 +43,11 @@ class UserController {
       "SELECT * FROM users WHERE email = (?)",
       [email]
     );
-    
 
     if (userWithUpdatedEmail.id !== user.id) {
       throw new AppError("Este e-email já está em uso.");
     }
 
-   
     // nullish operator , se o name = vazio, então utiliza o valor de user.name
     user.name = name ?? user.name;
     user.email = email ?? user.email;
--- src/server.js
+++ src/server.js
@@ -33,5 +33,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
